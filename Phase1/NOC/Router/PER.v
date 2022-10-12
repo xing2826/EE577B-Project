fd37e7fc6036caf37fd391fbeefc79601907a4a7
Xin Xu <xxu26721@usc.edu>
2022-10-12 14:31:09 -0700

    router for PE

---
+++ Phase1/NOC/Router/PER.v
@@ -0,0 +1,8 @@
+// The input and output PE channel use the same structual
+//hence only one module designed below and instantiate 2 time in gold_router.v
+
+module PER (
+    ports
+);
+    
+endmodule
