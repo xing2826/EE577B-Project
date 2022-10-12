30a03cc9f10c012d5431be476e2c846281470a14
Xin Xu <xxu26721@usc.edu>
2022-10-12 14:29:33 -0700

    Revert "create hierarchy"
    This reverts commit fc24e513055a314ae165ca2704955dffe9aacce6.

--- Phase1/NOC/Router/PER.v
+++
@@ -1,8 +0,0 @@
-// The input and output PE channel use the same structual
-//hence only one module designed below and instantiate 2 time in gold_router.v
-
-module PER (
-    ports
-);
-    
-endmodule
