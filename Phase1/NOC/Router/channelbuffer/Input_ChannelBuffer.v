4a8ece7b5292056d3a4afed24bea42078b88e692
Xin Xu <xxu26721@usc.edu>
2022-10-13 17:48:35 -0700

    added tb

--- Phase1/NOC/Router/channelbuffer/Input_ChannelBuffer.v
+++ Phase1/NOC/Router/channelbuffer/Input_ChannelBuffer.v
@@ -4,11 +4,11 @@
 module Input_ChannelBuffer (
     input clk,
     input reset,
-    input [63:0] di; 
+  input [63:0] di, 
     input WE, //write enable signal for the buffer
-    output reg [63:0] packet;
+  output reg [63:0] packet,
     output reg request,  //request for grant
-    output reg ToPE; //if the packet is decoded and found with hop value of 0, then ToPE changes to 1;
+    output reg ToPE //if the packet is decoded and found with hop value of 0, then ToPE changes to 1;
 );
     always @ (posedge clk) begin
         if (reset) 
@@ -18,9 +18,11 @@ module Input_ChannelBuffer (
     end
 
     always @ (packet) begin
-        if (|packet == 1) 
+      if (|packet == 1) begin
         request <= 1; // blocking or non-blocking here?
-        if (packet[55:48] == 0;)
+        if (packet[55:48] == 8'b00000000)
         ToPE <= 1; //same as above, blcoking or non-blocking?
+        else ToPE <= 0;
+      end
     end
 endmodule
