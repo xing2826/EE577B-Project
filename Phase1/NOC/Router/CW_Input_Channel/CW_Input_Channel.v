d5b68b6925ecb5ed33c20ed0121a4c3a38e0184a
Xin Xu <xxu26721@usc.edu>
2022-10-15 12:06:12 -0700

    1

---
+++ Phase1/NOC/Router/CW_Input_Channel/CW_Input_Channel.v
@@ -0,0 +1,8 @@
+// This is input channel for Clock Wise, which contains 2 virtual channel (input buffer)
+`include "./ channelbuffer/ Input_ChannelBuffer.v"
+
+module CW_Input_Channel (
+    ports
+);
+    
+endmodule
