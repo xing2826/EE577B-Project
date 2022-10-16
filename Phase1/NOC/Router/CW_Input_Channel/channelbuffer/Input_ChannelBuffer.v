3c982e24ce3c806634f1d2e12761e508fb4846cc
Xin Xu <xxu26721@usc.edu>
2022-10-15 17:59:09 -0700

    1

--- Phase1/NOC/Router/CW_Input_Channel/channelbuffer/Input_ChannelBuffer.v
+++ Phase1/NOC/Router/CW_Input_Channel/channelbuffer/Input_ChannelBuffer.v
@@ -6,28 +6,21 @@ module Input_ChannelBuffer (
   input reset,
   input [63:0] di, 
   input WE, //write enable signal for the buffer
-  output reg [63:0] packet,
-  output reg request,  //request for grant
-  output reg ToPE //if the packet is decoded and found with hop value of 0, then ToPE changes to 1;
+  input RE,
+  output reg [63:0] buffer,
+  output reg full
 );
 
   always @ (posedge clk) begin
-    if (reset) 
-    packet <= 64'b0;
-    else if (WE)
-    packet <= di;
+    if (reset) begin
+      buffer <= 64'b0;
+      full <= 0;
+    end 
+    else if (RE) full <= 0;
+    else if (WE) begin
+      buffer <= di;
+      full <= 1;
+    end 
   end
 
-  always @ (packet) begin
-    if (|packet == 1) begin
-      request <= 1; // blocking or non-blocking here?
-      if (packet[55:48] == 8'h00)
-        ToPE <= 1; //same as above, blcoking or non-blocking?
-      else
-        ToPE <= 0;
-    end
-    else if (|packet == 0)
-      request <= 0;
-      
-  end
 endmodule
